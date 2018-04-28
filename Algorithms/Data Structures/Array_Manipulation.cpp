fd2785fafd38fe15bb4b7188e91bb0a4f66b1a56
sbty <shibata.youhei@gmail.com>
2018-04-29 00:07:14 +0900

    day17 mathematics

---
+++ Algorithms/Data Structures/Array_Manipulation.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+/*
+ * Complete the arrayManipulation function below.
+ */
+int arrayManipulation(int n, vector<vector<int>> queries) {
+    /*
+     * Write your code here.
+     */
+    
+    return 0;
+}
+
+int main()
+{
+     long int N,K,p,q,sum,i,j,max=0,x=0;
+
+    cin>>N>>K;
+    long int *a=new long int[N+1]();
+
+    for(i=0;i<K;i++)
+    {
+        cin>>p>>q>>sum;
+        a[p]+=sum;
+        if((q+1)<=N) a[q+1]-=sum;
+    }
+
+    for(i=1;i<=N;i++)
+        cout << a[i] << endl;
+    
+    for(i=1;i<=N;i++)
+    {
+       x=x+a[i];
+       if(max<x) max=x;
+
+    }
+
+    cout<<max;
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
